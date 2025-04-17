04c72dd2037cf08c11c87395e38535a85d3bfd27
이순용 <ssun@isun-yong-ui-MacBookPro.local>
2025-04-17 15:41:02 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6a00",
+        dark: "#0d0d0d",
+        graydark: "#1a1a1a",
+      },
+    },
+  },
+  plugins: [],
+}
